3c2b7f4ecc3c498ebcac0ba4f98343f234ce45a5
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-01-31 19:06:55 +0100

    BaseTest

---
+++ src/test/java/Tests/BaseTest.java
@@ -0,0 +1,48 @@
+package Tests;
+
+import Pages.BasePage;
+import Pages.HomePage;
+import Pages.LoginPage;
+import Pages.SignupPage;
+import org.junit.AfterClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.security.Signature;
+import java.time.Duration;
+
+public abstract class BaseTest {
+    protected WebDriver driver;
+    protected WebDriverWait driverWait;
+    protected LoginPage loginPage;
+
+    protected SignupPage signupPage;
+    protected BasePage basePage;
+
+    protected HomePage homePage;
+
+    protected static final String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", "D:\\driver chromechrome\\driver.exe");
+        driver = new ChromeDriver();
+        driverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        loginPage = new LoginPage(driver, driverWait);
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        driver.get("https://vue-demo.daniel-avellaneda.com/");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        driver.quit();
+    }
+
+
+}
