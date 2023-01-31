c8a44bacd088587ed24d8e0b4071c012485bb57d
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-02-01 00:03:47 +0100

    HomePage update

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,23 +5,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 public class HomePage extends BasePage {
-    @FindBy(xpath = "/html/body/div/div/main/div/div[2]/div/div[2]/div[1]/a/span")
-    private WebElement buyMeACoffeButton;
+//    @FindBy(xpath = "/html/body/div/div/main/div/div[2]/div/div[2]/div[1]/a/span")
+//    private WebElement buyMeACoffeButton;
+//
+//    @FindBy(xpath = "/html/body/div/div/div/header/div/div[3]/div[1]/div/div/div/div[1]/div/div[1]")
+//    private WebElement lightToDark;
+//
+//    @FindBy(xpath = "/html/body/div/div/div/header/div/div[3]/div[1]/div/div/div/div[1]/div/div[1]")
+//    private WebElement darkToLight;
 
-    @FindBy(xpath = "/html/body/div/div/div/header/div/div[3]/div[1]/div/div/div/div[1]/div/div[1]")
-    private WebElement lightToDark;
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]")
+    private WebElement logoutButton;
 
-    @FindBy(xpath = "/html/body/div/div/div/header/div/div[3]/div[1]/div/div/div/div[1]/div/div[1]")
-    private WebElement DarkToLight;
 
 
     public HomePage(WebDriver driver, WebDriverWait driverWait) {
         super(driver, driverWait);
     }
 
-    public void changeThemeToDark() {
-        lightToDark.click();
+    public void logout() {
+        logoutButton.click();
     }
 
-
+    public WebElement getLogoutButton() {
+        return logoutButton;
+    }
 }
