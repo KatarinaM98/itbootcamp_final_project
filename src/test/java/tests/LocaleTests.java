f90eed94961f88a46c39f3553c80fd00d2effe47
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-02-02 21:49:24 +0100

    LocaleTests

---
+++ src/test/java/tests/LocaleTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class LocaleTests extends BaseTest{
+
+    @Test
+    public void test1_Set_locale_to_ES() {
+        
+
+    }
+
+}
