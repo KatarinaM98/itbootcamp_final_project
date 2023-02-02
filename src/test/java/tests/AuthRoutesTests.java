40e9cdf2bd379f9c72cef95e35082a5d103fdedb
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-02-02 21:46:28 +0100

    AuthRoutesTests update Test3: Forbids visits to admin cities url if not authenticated Test4: Forbids visits to admin users url if not authenticated

--- src/test/java/tests/AuthRoutesTests.java
+++ src/test/java/tests/AuthRoutesTests.java
@@ -12,25 +12,42 @@ import pages.HomePage;
 
 import java.util.List;
 
-public class AuthRoutesTests extends BaseTest{
+public class AuthRoutesTests extends BaseTest {
 
     @Test
-    public void Test_1_Forbids_visits_to_home_url_if_not_authenticated(){
+    public void Test_1_Forbids_visits_to_home_url_if_not_authenticated() {
         driver.navigate().to("https://vue-demo.daniel-avellaneda.com/home");
         driverWait.until(ExpectedConditions.urlContains("/login"));
         String actualLink = driver.getCurrentUrl();
         Assert.assertTrue(actualLink.contains("/login"));
     }
 
-
-
     @Test
-    public void Test2_Forbids_visits_to_profile_url_if_not (){
+    public void Test2_Forbids_visits_to_profile_url_if_not() {
         driver.navigate().to("https://vue-demo.daniel-avellaneda.com/profile");
-        driverWait.until(ExpectedConditions.urlContains("/profile"));
+        driverWait.until(ExpectedConditions.urlContains("/login"));
         String actualLink = driver.getCurrentUrl();
         Assert.assertTrue(actualLink.contains("/login"));
     }
+
+    @Test
+    public void Test3_Forbids_visits_to_admin_cities_url_if_not_authenticated() {
+        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        driverWait.until(ExpectedConditions.urlContains("/login"));
+        String actualLink = driver.getCurrentUrl();
+        Assert.assertTrue(actualLink.contains("/login"));
+    }
+
+
+    @Test
+    public void Test4_Forbids_visits_to_admin_users_url_if_not_authenticated() {
+        driver.navigate().to("https://vue-demo.daniel-avellaneda.com/admin/users");
+        driverWait.until(ExpectedConditions.urlContains("/login"));
+        String actualLink = driver.getCurrentUrl();
+        Assert.assertTrue(actualLink.contains("/login"));
+    }
+
+
     @AfterMethod
     public void afterMethod() {
         List<WebElement> logoutButton = driver.findElements(By.xpath("//*[@id=\"app\"]/div/div/header/div/div[3]/button[2]/span"));
