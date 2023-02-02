532c04584b683ba44fb7331647773577fc3ecbda
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-02-02 01:40:24 +0100

    AdminCities page AdminCitiesTests

--- src/test/java/pages/AdminCities.java
+++ src/test/java/pages/AdminCities.java
@@ -1,16 +1,152 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Factory;
 
 public class AdminCities extends BasePage {
 
-    @FindBy
+
+   // @FindBy
+   @FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]/span")
+   private WebElement logoutButton;
+
+    @FindBy(xpath = "/html/body/div/div[1]/main/div/div[2]/div/div[1]/div[1]/div[3]/form/div[1]/button/span")
+    private WebElement newItemButton;
+
+    @FindBy(id = "name")
+    private WebElement cityName;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[3]/div/div/div[3]/button[2]/span")
+    private WebElement saveButton;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]")
+    private WebElement message;
+
+    @FindBy(xpath = "//*[@id=\"edit\"]/span/i")
+    private WebElement edit;
+
+    @FindBy(id = "search")
+    private WebElement searchText;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[2]/table/tbody/tr[1]/td[2]")
+    private WebElement editedCityInTable;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[2]/table/tbody/tr/td[2]")
+    private WebElement searchedCityInTable;
+
+    @FindBy(xpath = "//*[@id=\"delete\"]/span/i")
+    private WebElement delete;
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[6]/div/div")
+    private WebElement deleteDialog;
+    //*[@id="app"]/div[6]
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[4]/div/div/div[2]/button[2]")
+    private WebElement delete2;
+
+
+
+    @FindBy(xpath = "//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[2]/div/div/div/div/div[1]/ul/li")
+    private WebElement message2;
+
+
+
+
 
     public AdminCities (WebDriver driver, WebDriverWait driverWait) {
+
         super(driver, driverWait);
+
+    }
+
+    public void openCitiesPage () {
+
+
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+
+    }
+
+    public void addNewCity (String city) {
+
+        newItemButton.click();
+        cityName.sendKeys(city);
+        saveButton.click();
+
+    }
+
+    public void editCity () {
+
+        edit.click();
+        //Ako se stavi "+-edited" onda test 4 fejluje jer ne može da pretražuje "+-" karaktere
+        cityName.sendKeys(" edited");
+        saveButton.click();
+
+    }
+
+    public void searchCity () {
+
+        searchText.sendKeys(editedCityInTable.getText());
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    public void deleteCity () {
+
+        delete.click();
+        //Ako se stavi "+-edited" onda test 4 fejluje jer ne može da pretražuje "+-" karaktere
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[6]/div/div/div[2]/button[2]/span")));
+        delete2.click();
+
+
+
+
+    }
+
+
+
+
+    public WebElement getLogoutButton() {
+        return logoutButton;
+    }
+
+    public WebElement getNewItemButton() {
+        return newItemButton;
+    }
+
+    public WebElement getMessage() {
+        return message;
+    }
+
+    public WebElement getEditedCityInTable() {
+        return editedCityInTable;
+    }
+
+    public WebElement getSearchedCityInTable() {
+        return searchedCityInTable;
+    }
+
+    public WebElement getSearchText() {
+        return searchText;
+    }
+
+    public WebElement getMessage2() {
+        return message2;
     }
 }
 
--- src/test/java/tests/AdminCitiesTests.java
+++ src/test/java/tests/AdminCitiesTests.java
@@ -1,5 +1,122 @@
 package tests;
 
-public class AdminCitiesTests extends BaseTest{
-    
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class AdminCitiesTests extends BaseTest {
+
+    @Test
+    public void test1_Visits_the_admin_cities_page_and_list_cities() {
+        loginPage.openLoginPage();
+        loginPage.login("admin@admin.com", "12345");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        adminCities.openCitiesPage();
+        String actualLink = driver.getCurrentUrl();
+        Assert.assertTrue(actualLink.contains("/admin/cities"));
+        driver.manage().window().maximize();
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/div/header/div/div[3]/button[2]/span")));
+        Assert.assertTrue(adminCities.getLogoutButton().isDisplayed());
+    }
+
+
+    @Test
+    public void test2_Create_new_city() {
+        Faker faker = new Faker();
+        loginPage.openLoginPage();
+        loginPage.login("admin@admin.com", "12345");
+        driverWait.until(ExpectedConditions.urlContains("/home"));
+        adminCities.openCitiesPage();
+
+        adminCities.addNewCity(faker.address().city());
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]")));
+        /*
+        Test prolazi ako se u expected napiše:
+         "Saved successfully\n" +
+                "CLOSE"
+         Test ne prolazi ako se napiše "Saved successfully"
+        */
+        //Assert.assertEquals("Saved successfully", adminCities.getMessage().getText());
+        Assert.assertEquals("Saved successfully\n" + "CLOSE", adminCities.getMessage().getText());
+
+    }
+
+    @Test (dependsOnMethods = { "test2_Create_new_city" })
+    public void test3_Edit_city() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+//        loginPage.openLoginPage();
+//        loginPage.login("admin@admin.com", "12345");
+//        driverWait.until(ExpectedConditions.urlContains("/home"));
+//        adminCities.openCitiesPage();
+        //driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"edit\"]/span/i")));
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        adminCities.editCity();
+
+        /*
+        Test prolazi ako se u expected napiše:
+        "Saved successfully\n" +
+                "CLOSE"
+         Test ne prolazi ako se napiše "Saved successfully"
+
+        */
+
+        //Assert.assertEquals("Saved successfully", adminCities.getMessage().getText());
+        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[3]/div/div/div/div/div[1]")));
+
+        Assert.assertEquals("Saved successfully\n" + "CLOSE", adminCities.getMessage().getText());
+    }
+
+    @Test (dependsOnMethods = { "test3_Edit_city", "test2_Create_new_city" })
+    public void test4_Search_city() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        //Ako se stavi "+-edited" onda test 4 fejluje jer ne može da pretražuje "+-" karaktere --- PRIJAVITI BUG
+        adminCities.searchCity();
+        Assert.assertEquals(adminCities.getEditedCityInTable().getText(), adminCities.getSearchedCityInTable().getText());
+    }
+
+    @Test (dependsOnMethods = { "test3_Edit_city", "test2_Create_new_city", "test4_Search_city" })
+    public void test5_Delete_city() {
+        driver.get("https://vue-demo.daniel-avellaneda.com/admin/cities");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        adminCities.searchCity();
+        //driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[6]/div/div/div[2]/button[2]/span")));
+
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Assert.assertEquals(adminCities.getSearchText().getAttribute("value"), adminCities.getSearchedCityInTable().getText());
+
+        adminCities.deleteCity();
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[2]/div/div/div/div/div[1]/ul/li")));
+
+        Assert.assertEquals("Not found", adminCities.getMessage2().getText());
+
+    }
+
+
+
+
 }
