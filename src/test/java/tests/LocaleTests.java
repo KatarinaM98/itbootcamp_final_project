f1708bd06653241c98a465bda9ea49dbbf2e6011
KatarinaM98 <milojevic.katarinanina@gmail.com>
2023-02-07 19:36:06 +0100

    LocaleTests update

--- src/test/java/tests/LocaleTests.java
+++ src/test/java/tests/LocaleTests.java
@@ -1,8 +1,6 @@
 package tests;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class LocaleTests extends BaseTest{
@@ -10,33 +8,23 @@ public class LocaleTests extends BaseTest{
     @Test
     public void test1_Set_locale_to_ES() {
         homePage.changeLanguage("ES");
-
-        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")));
-
-        Assert.assertEquals("Página de aterrizaje", homePage.getHeaderTextES().getText());
-
+        homePage.driverWaitForHeading();
+        Assert.assertEquals("Página de aterrizaje", homePage.getHeaderText().getText());
     }
 
     @Test
     public void test2_Set_locale_to_EN() {
         homePage.changeLanguage("EN");
-
-        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")));
-
-        Assert.assertEquals("Landing", homePage.getHeaderTextES().getText());
-
+        homePage.driverWaitForHeading();
+        Assert.assertEquals("Landing", homePage.getHeaderText().getText());
     }
 
     @Test
     public void test3_Set_locale_to_FR() {
         homePage.changeLanguage("FR");
-
-        driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div[1]/div[1]/h1")));
-
-        Assert.assertEquals("Page d'atterrissage", homePage.getHeaderTextES().getText());
+        homePage.driverWaitForHeading();
+        Assert.assertEquals("Page d'atterrissage", homePage.getHeaderText().getText());
 
     }
-
-
 
 }
